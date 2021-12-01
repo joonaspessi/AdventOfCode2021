9bff638e35980c862b16c9e28be6ee2ecbb04030
Joonas Pessi <joonas.pessi@gmail.com>
2021-12-01 19:12:10 +0200

    use rust analyzer

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,57 +1,57 @@
 const INPUT_FILE: &str = include_str!("../../inputs/day01.txt");
 
 fn input_to_numbers(input: String) -> Vec<usize> {
-  input
-    .split('\n')
-    .map(|i| i.parse::<usize>().expect("parsing"))
-    .collect()
+    input
+        .split('\n')
+        .map(|i| i.parse::<usize>().expect("parsing"))
+        .collect()
 }
 
 fn part_1(file: String) -> usize {
-  input_to_numbers(file)
-    .windows(2)
-    .filter(|values| values[0] < values[1])
-    .count()
+    input_to_numbers(file)
+        .windows(2)
+        .filter(|values| values[0] < values[1])
+        .count()
 }
 
 fn part_2(file: String) -> usize {
-  input_to_numbers(file)
-    .windows(3)
-    .map(|values| values.iter().sum())
-    .collect::<Vec<usize>>()
-    .windows(2)
-    .filter(|values| values[0] < values[1])
-    .count()
+    input_to_numbers(file)
+        .windows(3)
+        .map(|values| values.iter().sum())
+        .collect::<Vec<usize>>()
+        .windows(2)
+        .filter(|values| values[0] < values[1])
+        .count()
 }
 
 fn main() {
-  let res1 = part_1(String::from(INPUT_FILE));
-  let res2 = part_2(String::from(INPUT_FILE));
-  println!("part 1: {}", res1);
-  println!("part 2: {}", res2);
+    let res1 = part_1(String::from(INPUT_FILE));
+    let res2 = part_2(String::from(INPUT_FILE));
+    println!("part 1: {}", res1);
+    println!("part 2: {}", res2);
 }
 
 #[cfg(test)]
 mod test {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn test_solves_part1() {
-    assert_eq!(
-      part_1(String::from(
-        "199\n200\n208\n210\n200\n207\n240\n269\n260\n263"
-      )),
-      7
-    );
-  }
+    #[test]
+    fn test_solves_part1() {
+        assert_eq!(
+            part_1(String::from(
+                "199\n200\n208\n210\n200\n207\n240\n269\n260\n263"
+            )),
+            7
+        );
+    }
 
-  #[test]
-  fn test_solves_part2() {
-    assert_eq!(
-      part_2(String::from(
-        "199\n200\n208\n210\n200\n207\n240\n269\n260\n263"
-      )),
-      5
-    );
-  }
+    #[test]
+    fn test_solves_part2() {
+        assert_eq!(
+            part_2(String::from(
+                "199\n200\n208\n210\n200\n207\n240\n269\n260\n263"
+            )),
+            5
+        );
+    }
 }
