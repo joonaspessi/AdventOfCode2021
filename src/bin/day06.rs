7e5c6a289511442396208bb990c8fb210bda28c8
Joonas Pessi <joonas.pessi@cargotec.com>
2021-12-06 14:58:19 +0200

    Day06 part02

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -56,6 +56,39 @@ fn part_1(input: String) -> u32 {
     fishes.len() as u32
 }
 
+fn parse_2(input: String) -> HashMap<u32, u64> {
+    let mut fishes: HashMap<u32, u64> = HashMap::new();
+    let ages: Vec<u32> = input
+        .trim()
+        .split(',')
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect();
+    for age in ages {
+        *fishes.entry(age).or_insert(0) += 1;
+    }
+
+    fishes
+}
+
+fn part_2(input: String) -> u64 {
+    let mut fishes = parse_2(input);
+    for _day in 1..=256 {
+        let mut new_fish_generation = HashMap::new();
+
+        for (age, count) in fishes {
+            if age == 0 {
+                *new_fish_generation.entry(8).or_insert(0) += count;
+                *new_fish_generation.entry(6).or_insert(0) += count;
+            } else {
+                *new_fish_generation.entry(age - 1).or_insert(0) += count;
+            }
+        }
+        fishes = new_fish_generation;
+    }
+
+    fishes.iter().fold(0, |sum, (_age, count)| sum + count) as u64
+}
+
 fn main() {
     let res_1 = part_1(INPUT_FILE.to_string());
     println!("part1: {}", res_1);
@@ -73,5 +106,15 @@ mod test {
     #[test]
     fn test_solves_part_1_input() {
         assert_eq!(part_1(String::from(INPUT_FILE)), 352151);
+    }
+
+    #[test]
+    fn test_solves_part_2_example() {
+        assert_eq!(part_2(String::from("3,4,3,1,2")), 26984457539);
+    }
+
+    #[test]
+    fn test_solves_part_2_input() {
+        assert_eq!(part_2(String::from(INPUT_FILE)), 1601616884019)
     }
 }
