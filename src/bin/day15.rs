05cb16c5373174ecb993f79f8e609fd2ae7748be
Joonas Pessi <joonas.pessi@cargotec.com>
2021-12-25 14:20:28 +0200

    add dijkstra with adjacency list and heap

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -1,4 +1,68 @@
+use std::{cmp::Ordering, collections::BinaryHeap, usize};
+
 const INPUT_FILE: &str = include_str!("../../inputs/day15.txt");
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct State {
+    cost: usize,
+    position: usize,
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other
+            .cost
+            .cmp(&self.cost)
+            .then_with(|| self.position.cmp(&other.position))
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+struct Edge {
+    node: usize,
+    cost: usize,
+}
+
+fn dijkstra_adj_list_heap(adj_list: &Vec<Vec<Edge>>, start: usize, goal: usize) -> Option<usize> {
+    let mut dist: Vec<_> = (0..adj_list.len()).map(|_| usize::MAX).collect();
+
+    let mut heap = BinaryHeap::new();
+
+    dist[start] = 0;
+
+    heap.push(State {
+        cost: 0,
+        position: start,
+    });
+
+    while let Some(State { cost, position }) = heap.pop() {
+        if position == goal {
+            return Some(cost);
+        }
+
+        if cost > dist[position] {
+            continue;
+        }
+
+        for edge in &adj_list[position] {
+            let next = State {
+                cost: cost + edge.cost,
+                position: edge.node,
+            };
+
+            if next.cost < dist[next.position] {
+                heap.push(next);
+                dist[next.position] = next.cost;
+            }
+        }
+    }
+    None
+}
 
 fn parse(input: String, part2: bool) -> Vec<Vec<u32>> {
     let mut parsed: Vec<Vec<u32>> = input
