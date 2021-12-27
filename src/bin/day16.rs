5fbd9e305dfcfce2b67f15200e3cc274594589f6
Joonas Pessi <joonas.pessi@cargotec.com>
2021-12-27 17:29:33 +0200

    day16 part02

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,5 +1,61 @@
 const INPUT_FILE: &str = include_str!("../../inputs/day16.txt");
 
+enum Operator {
+    Sum(usize),
+    Product(usize),
+    Minimum(Vec<usize>),
+    Maximum(Vec<usize>),
+    GreaterThan(Vec<usize>),
+    LessThan(Vec<usize>),
+    EqualTo(Vec<usize>),
+}
+
+impl Operator {
+    fn insert(&mut self, value: usize) {
+        match self {
+            Operator::Sum(v) => {
+                *v += value;
+            }
+            Operator::Product(v) => {
+                *v *= value;
+            }
+            Operator::Minimum(v)
+            | Operator::Maximum(v)
+            | Operator::GreaterThan(v)
+            | Operator::LessThan(v)
+            | Operator::EqualTo(v) => v.push(value),
+        }
+    }
+    fn evaluate(&self) -> usize {
+        match self {
+            Operator::Sum(v) | Operator::Product(v) => *v,
+            Operator::Minimum(v) => *v.iter().min().unwrap(),
+            Operator::Maximum(v) => *v.iter().max().unwrap(),
+            Operator::GreaterThan(v) => {
+                if v[0] > v[1] {
+                    1
+                } else {
+                    0
+                }
+            }
+            Operator::LessThan(v) => {
+                if v[0] < v[1] {
+                    1
+                } else {
+                    0
+                }
+            }
+            Operator::EqualTo(v) => {
+                if v[0] == v[1] {
+                    1
+                } else {
+                    0
+                }
+            }
+        }
+    }
+}
+
 fn parse(input: String) -> Vec<char> {
     let mut decoded = String::new();
     for c in input.trim().chars() {
@@ -50,16 +106,76 @@ fn parse_packet(index: &mut usize, decoded: &[char]) -> usize {
     version_sum
 }
 
+fn parse_packet_2(index: &mut usize, decoded: &[char]) -> usize {
+    // Headers
+    let version = binary_chars_to_number(&decoded[*index..*index + 3]);
+    let type_id = binary_chars_to_number(&decoded[*index + 3..*index + 6]);
+    *index += 6;
+
+    if type_id == 4 {
+        // Literal
+        let mut continues = true;
+        let mut val = vec![];
+        while continues {
+            continues = decoded[*index] == '1';
+            let mut decoded: Vec<char> = decoded[*index + 1..*index + 5].iter().copied().collect();
+            val.append(&mut decoded);
+            *index += 5
+        }
+        let literal_value = binary_chars_to_number(&val);
+        println!("literal: {}", literal_value);
+        literal_value
+    } else {
+        // Operator
+        let mut operator = match type_id {
+            0 => Operator::Sum(0),
+            1 => Operator::Product(1),
+            2 => Operator::Minimum(vec![]),
+            3 => Operator::Maximum(vec![]),
+            5 => Operator::GreaterThan(vec![]),
+            6 => Operator::LessThan(vec![]),
+            7 => Operator::EqualTo(vec![]),
+            _ => panic!("unrecognized operator"),
+        };
+
+        let length_type_id = decoded[*index];
+        *index += 1;
+
+        if length_type_id == '0' {
+            let subpackage_len = binary_chars_to_number(&decoded[*index..*index + 15]);
+            *index += 15;
+            let parse_until = *index + subpackage_len;
+
+            while *index < parse_until {
+                operator.insert(parse_packet_2(index, decoded));
+            }
+        } else {
+            let subpackage_len = binary_chars_to_number(&decoded[*index..*index + 11]);
+            *index += 11;
+            for _ in 0..subpackage_len {
+                operator.insert(parse_packet_2(index, decoded));
+            }
+        }
+        operator.evaluate()
+    }
+}
+
 fn part_1(input: String) -> usize {
     let decoded = parse(input);
     let mut index = 0;
-    println!("part1: {:?}", decoded);
     parse_packet(&mut index, &decoded)
 }
 
+fn part_2(input: String) -> usize {
+    let decoded = parse(input);
+    let mut index = 0;
+    parse_packet_2(&mut index, &decoded)
+}
 fn main() {
     let res1 = part_1(INPUT_FILE.to_string());
     println!("part1: {}", res1);
+    let res2 = part_2(INPUT_FILE.to_string());
+    println!("part2: {}", res2);
 }
 
 #[cfg(test)]
@@ -80,5 +196,21 @@ mod test {
     #[test]
     fn solves_part1_input() {
         assert_eq!(part_1(INPUT_FILE.to_string()), 965);
+    }
+
+    #[test]
+    fn solves_part2_example() {
+        assert_eq!(part_2("C200B40A82".to_string()), 3);
+        assert_eq!(part_2("04005AC33890".to_string()), 54);
+        assert_eq!(part_2("880086C3E88112".to_string()), 7);
+        assert_eq!(part_2("CE00C43D881120".to_string()), 9);
+        assert_eq!(part_2("F600BC2D8F".to_string()), 0);
+        assert_eq!(part_2("D8005AC2A8F0".to_string()), 1);
+        assert_eq!(part_2("9C0141080250320F1802104A08".to_string()), 1);
+    }
+
+    #[test]
+    fn solves_part2_input() {
+        assert_eq!(part_2(INPUT_FILE.to_string()), 116672213160);
     }
 }
