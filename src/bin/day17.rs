a6f04c9a5731b8f32ddddc0690e2a62019501422
Joonas Pessi <joonas.pessi@cargotec.com>
2021-12-28 22:41:06 +0200

    Day17 part01

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -14,10 +14,62 @@ fn parse(input: String) -> (i64, i64, i64, i64) {
     )
 }
 
+fn is_target_hit(xv: i64, yv: i64, min_x: i64, max_x: i64, min_y: i64, max_y: i64) -> Option<i64> {
+    let mut x = 0;
+    let mut y = 0;
+    let mut x_vel = xv;
+    let mut y_vel = yv;
+
+    let mut max_y_trajectory_height = 0;
+    loop {
+        x += x_vel;
+        y += y_vel;
+
+        max_y_trajectory_height = std::cmp::max(max_y_trajectory_height, y);
+
+        if x >= min_x && x <= max_x && y >= min_y && y <= max_y {
+            return Some(max_y_trajectory_height);
+        }
+
+        if x > max_x || y < min_y {
+            return None;
+        }
+
+        x_vel = std::cmp::max(x_vel - 1, 0);
+        y_vel -= 1;
+    }
+}
+
 fn part_1(input: String) -> i64 {
     let (min_x, max_x, min_y, max_y) = parse(input);
-    println!("{} {} {} {}", min_x, max_x, min_y, max_y);
-    1
+
+    // Determine minimum x velocity
+    // Use sum-first-n-natural-numbers equation
+    // min_x = n * (n + 1) / 2
+    // Solve n aka velocity from the equation
+    // n^2 + n - 2min_x = 0
+    // n = (-1 +- sqrt(1 - 4 * 1 * -2min_x) ) / 2
+    // n = (-1 +- sqrt(1 + 8min_x)) / 2
+    let velocity_x_min = ((-1f64 + (1f64 + 8f64 * min_x as f64).sqrt()) / 2f64).ceil() as i64;
+    let velocity_x_max = max_x;
+
+    // Just too tired right now, Probably under this range.
+    let velocity_y_min = 2 * min_y;
+    let velocity_y_max = -2 * (min_y - 1);
+
+    let mut max_y_trajectory_height = 0;
+
+    for xv in velocity_x_min..velocity_x_max {
+        for yv in velocity_y_min..=velocity_y_max {
+            if let Some(y_hit) = is_target_hit(xv, yv, min_x, max_x, min_y, max_y) {
+                if y_hit > max_y_trajectory_height {
+                    max_y_trajectory_height = y_hit
+                }
+            }
+        }
+    }
+
+    max_y_trajectory_height
 }
 
 fn main() {
@@ -31,6 +83,11 @@ mod test {
 
     #[test]
     fn solves_part1_example() {
-        assert_eq!(part_1("target area: x=20..30, y=-10..-5 ".to_string()), 0);
+        assert_eq!(part_1("target area: x=20..30, y=-10..-5 ".to_string()), 45);
+    }
+
+    #[test]
+    fn solves_part1_input() {
+        assert_eq!(part_1(INPUT_FILE.to_string()), 4186);
     }
 }
